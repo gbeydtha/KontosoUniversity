8b15fb1e1694538bce241cc223dc961d3eb69508
Abul Hasan <beydtha@gmail.com>
2020-02-24 23:51:45 +0100

    Add project files.

---
+++ KontosoUniversity/DAL/UniversityContext.cs
@@ -0,0 +1,27 @@
+﻿using KontosoUniversity.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace KontosoUniversity.DAL
+{
+    public class UniversityContext : DbContext
+    {
+        public UniversityContext() : base("UniversityContext")
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ KontosoUniversity/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KontosoUniversity.MvcApplication" Language="C#" %>
